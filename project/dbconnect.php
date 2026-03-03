f15438f9d7bdcba6ee679fda4ad418e7292c0440
Koumolou <learningsin2024@gmail.com>
2026-03-03 15:36:40 +0000

    Deploy db

--- project/dbconnect.php
+++ project/dbconnect.php
@@ -2,10 +2,15 @@
 require_once __DIR__ . '/env.php';
 loadEnv();
 
-// temporary debug - remove after fixing
-var_dump([
-    'DB_HOST' => getenv('DB_HOST'),
-    'DB_USER' => getenv('DB_USER'),
-    'DB_NAME' => getenv('DB_NAME'),
-]);
-die();
+$servername = $_ENV['DB_HOST'] ?? getenv("DB_HOST");
+$username   = $_ENV['DB_USER'] ?? getenv("DB_USER");
+$password   = $_ENV['DB_PASS'] ?? getenv("DB_PASS");
+$database   = $_ENV['DB_NAME'] ?? getenv("DB_NAME");
+
+var_dump(['DB_HOST' => $servername, 'DB_USER' => $username, 'DB_NAME' => $database]);
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+?>
